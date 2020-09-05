f925e5cf78affb9426b211e677834f094fd82f9a
unknown <zhutongtong@360.cn>
2020-09-05 13:20:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<div>hello</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
