dd7306c71d0954abc47419722aafe8333d0341b6
unknown <zhutongtong@360.cn>
2020-09-20 13:32:41 +0800

    使用watcheffect代替mounted和updated

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="gulu-tabs">
-    <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item" :class="{selected: t === selected}" v-for="(t,index) in titles"  @click="select(t)" :key="index">{{t}}</div>
+  <div class="gulu-tabs-nav" ref="container">
+    <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t === selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
        <component class="gulu-tabs-content-item" :is="current" :key="selected"/>
@@ -10,7 +11,11 @@
 </template>
 
 <script lang="ts">
-import { computed } from 'vue'
+import { 
+  computed,
+  ref,
+  watchEffect
+} from 'vue'
 export default {
   props: {
     selected: {
@@ -18,14 +23,31 @@ export default {
     }
   },
   setup(props, context) {
+    const selectedItem = ref < HTMLDivElement > (null)
+    const indicator = ref < HTMLDivElement > (null)
+    const container = ref < HTMLDivElement > (null)
+    watchEffect(() => {
+      const {
+        width
+      } = selectedItem.value.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      const {
+        left: left1
+      } = container.value.getBoundingClientRect()
+      const {
+        left: left2
+      } = selectedItem.value.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value.style.left = left + 'px'
+    })
+
     const childrenComponents =  context.slots.default()
-    
+
     const titles = childrenComponents.map((c, index) => {
       return c.props.title
     })
 
     const current = computed(() => {
-      console.log('重新 return')
       return childrenComponents.filter((tag) => {
         return tag.props.title === props.selected
       })[0]
@@ -39,7 +61,10 @@ export default {
       childrenComponents,
       titles,
       current,
-      select
+      select,
+      indicator,
+      container,
+      selectedItem
     } 
   }  
 }
@@ -54,6 +79,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -64,6 +90,15 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
